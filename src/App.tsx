59938d880c3fe62727cf30b6e981a69577deff7b
sivajeyabalan-s <sivajeyabalan.s@maghil.com>
2026-05-22 14:48:52 +0530

    updated bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Menu from './pages/Menu/Menu'
 import Banquet from './pages/Banquet/Banquet'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/banquet" element={<Banquet />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/banquet" element={<Banquet />} />
+      </Routes>
+    </>
   )
 }
 
